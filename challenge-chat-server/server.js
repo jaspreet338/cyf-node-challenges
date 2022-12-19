22bb36ddb93c33cf9cd6c218151d46152e3e16a9
jaspreet kaur <kaurjas1928@yahoo.com>
2022-12-19 18:20:56 +0000

    create new message in server

--- challenge-chat-server/server.js
+++ challenge-chat-server/server.js
@@ -1,5 +1,34 @@
 const express = require("express");
 const app = express();
-app.listen(3000, function () {
-  console.log("Server 3000");
+
+app.use(express.json());
+const port = 6060;
+
+const message = {
+  id: 0,
+  name: "Jas",
+  message: "Welcome to my chat server!",
+};
+
+const messages = [message];
+
+//create new message
+
+app.post("/messages", (req, res) => {
+  const { name, message } = req.body;
+
+  const newMessages = {
+    id: messages.length,
+    name,
+    message,
+  };
+
+  if (!newMessages.name || !newMessages.message) {
+    return res.status(400).json("Please include a name and message");
+  }
+  messages.push(newMessages);
+  res.send(messages);
+});
+app.listen(port, function () {
+  console.log(`Listen in http://localhost:${port}`);
 });
